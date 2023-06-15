1c4c13954ab493d6b624948c164b8303af0a4ad8
Masood Hasan Khan <masood_hasan@msn.com>
2023-06-15 16:02:52 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+import "bulma/css/bulma.css";
+import Banner from "./components/Banner";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "bulma/css/bulma.css";
+
+export default function Banner() {
+  return (
+    <header>
+      <h1>SHOE STORE</h1>
+    </header>
+  );
+}
